9141d39ff28ec9227e037fc179cf9c5e295dac8a
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-04-17 15:12:36 +0530

    Config Server Updated

--- demo-exchangeservice/src/main/java/com/mahendra/demo1/DemoExchangeserviceApplication.java
+++ demo-exchangeservice/src/main/java/com/mahendra/demo1/DemoExchangeserviceApplication.java
@@ -1,17 +1,17 @@
 package com.mahendra.demo1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class DemoConfig1Application {
+@EnableAutoConfiguration
+public class DemoExchangeserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoConfig1Application.class, args);
+		SpringApplication.run(DemoExchangeserviceApplication.class, args);
 	}
 
 }
---
+++ demo-exchangeservice/src/main/java/com/mahendra/demo1/HomeController.java
@@ -0,0 +1,17 @@
+package com.mahendra.demo1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  // Inject property USD into current variable
+	//@Value("${USD}")
+	private String rupeesToUsd;
+	
+	@GetMapping("/")
+	public String rate() {
+		return rupeesToUsd;
+	}
+}
